d2577bff551262867582c93d60f9f52c174a1c2b
Fahd Jibril <jibreelfahd@gmail.com>
2024-11-15 00:36:10 +0100

    Tweaked how error is been sent to frontend in getAllUsers controller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -118,7 +118,7 @@ exports.getAllUsers = async (req, res) => {
     res.status(200).json({ users, totalUsers: users.length });
   } catch (error) {
     console.error("Error fetching users:", error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ error: "Server error" });
   }
 };
 
